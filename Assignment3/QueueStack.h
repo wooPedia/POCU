4fd8978f695e7c3a4eb1664d731f22c984af959b
woohyeon Ahn <europeluv@gmail.com>
2020-03-10 16:01:09 +0900

    QueueStack class Enqueue Dequeue Peek test

--- Assignment3/QueueStack.h
+++ Assignment3/QueueStack.h
@@ -19,13 +19,13 @@ namespace assignment3
 	class QueueStack
 	{
 	public:
-		using queueStack = std::queue<std::stack<T> >;
+		using queueStack = std::queue<std::stack<T>>;
 
 		QueueStack() = delete;
 		QueueStack(size_t maxStackSize);
-		//QueueStack(const QueueStack& other);
-		//~QueueStack();
-		//QueueStack<T>& operator=(const QueueStack& rhs);
+		QueueStack(const QueueStack& other);
+		~QueueStack(); 
+		QueueStack<T>& operator=(const QueueStack& rhs);
 
 		void Enqueue(T number);
 		inline T Peek() const;
@@ -42,7 +42,17 @@ namespace assignment3
 		inline bool Empty() const;
 
 	private:
+
+		struct Statistic
+		{
+			T Sum;
+			double TmpSum;
+		};
+
+
 		queueStack mQueueStack;
+		size_t mMaxStackSize;
+		Statistic* mStatistics;
 	};
 
 	/*
@@ -53,26 +63,102 @@ namespace assignment3
 
 	template <typename T>
 	QueueStack<T>::QueueStack(size_t maxStackSize)
+		: mMaxStackSize(maxStackSize)
+		, mStatistics(new Statistic())
 	{
-
 	}
+
+	template <typename T>
+	QueueStack<T>::QueueStack(const QueueStack& other)
+		: mQueueStack(other.mQueueStack)
+		, mMaxStackSize(other.mMaxStackSize)
+	{
+		mStatistics = new Statistic();
+		mStatistics->Sum = other.mStatistics->Sum;
+		mStatistics->TmpSum = other.mStatistics->TmpSum;
+	}
+
+	template <typename T>
+	QueueStack<T>::~QueueStack()
+	{
+		delete mStatistics;
+	}
+
+	template <typename T>
+	QueueStack<T>& QueueStack<T>::operator=(const QueueStack& rhs)
+	{
+		if (this == &rhs)
+		{
+			return *this;
+		}
+
+		delete mStatistics;
+		mStatistics = new Statistic();
+
+		mQueueStack = rhs.mQueueStack;
+		mStatistics->Sum = rhs.mStatistics->Sum;
+		mStatistics->TmpSum = rhs.mStatistics->TmpSum;
+
+		return *this;
+	}
+	
 
 	template <typename T>
 	void QueueStack<T>::Enqueue(T number)
 	{
+		// 각 스택의 최대 크기가 0일 경우 요소를 추가하지 않습니다.
+		if (mMaxStackSize == 0)
+		{
+			return;
+		}
 
+		// 큐가 비었거나 첫 스택과 마지막 스택이 모두 꽉 찼을 경우 새 스택을 추가합니다.
+		if (mQueueStack.empty() ||
+			(mQueueStack.front().size() == mMaxStackSize && mQueueStack.back().size() == mMaxStackSize))
+		{
+			mQueueStack.emplace(std::stack<T>());
+		}
+
+		// 첫 스택이 비었다면 첫 스택에, 꽉 찼다면 마지막 스택에 저장합니다.
+		if (mQueueStack.front().size() == mMaxStackSize)
+		{
+			mQueueStack.back().push(number);
+		}
+		else
+		{
+			mQueueStack.front().push(number);
+		}
+
+		mStatistics->Sum += number;
+		mStatistics->TmpSum += number;
 	}
 
 	template <typename T>
 	inline T QueueStack<T>::Peek() const
 	{
-		return T();
+		assert(!mQueueStack.empty() && !mQueueStack.front().empty());
+
+		// 제일 첫 번째 스택의 top을 반환합니다.
+		return mQueueStack.front().top();
 	}
 
 	template <typename T>
 	T QueueStack<T>::Dequeue()
 	{
-		return T();
+		assert(!mQueueStack.empty() && !mQueueStack.front().empty());
+		
+		T front = mQueueStack.front().top();
+		mQueueStack.front().pop();
+		mStatistics->Sum -= front;
+		mStatistics->TmpSum -= front;
+
+		// 첫 스택이 비었다면 pop합니다.
+		if (mQueueStack.front().empty())
+		{
+			mQueueStack.pop();
+		}
+
+		return front;
 	}
 
 	template <typename T>
