db9cb819b5500533296d9aa7af1f54d0c4d6bf09
woohyeon Ahn <europeluv@gmail.com>
2020-03-16 17:49:31 +0900

    Lab9: First test

---
+++ Lab9/Game.cpp
@@ -0,0 +1,59 @@
+#include "Game.h"
+
+namespace lab9
+{
+	Game::Game(unsigned int seed, unsigned int poolSize)
+		: mPoolSize(poolSize)
+		, mIceCubePool(mPoolSize)
+	{
+		srand(seed);
+	}
+
+	Game::~Game()
+	{
+		for (auto it = mActiveGameObjects.begin(); it != mActiveGameObjects.end(); ++it)
+		{
+			delete (*it);
+		}
+
+		mActiveGameObjects.clear();
+	}
+
+	void Game::Spawn()
+	{
+		// Pool에서 IceCube 개체를 가져와 초기화 후 ActiveGameObjects에 추가합니다.
+		IceCube* iceCube = mIceCubePool.Get();
+		iceCube->Initialize(rand() % MAX_FRAME_COUNT_TO_LIVE + 1);
+		mActiveGameObjects.push_back(iceCube);
+	}
+
+	void Game::Update()
+	{
+		for (auto it = mActiveGameObjects.begin(); it != mActiveGameObjects.end();)
+		{
+			IceCube* iceCube = *it;
+			iceCube->Animate();
+
+			// pool에 개체를 반환합니다.
+			if (!iceCube->IsActive())
+			{
+				it = mActiveGameObjects.erase(it);
+				mIceCubePool.Return(iceCube);
+				continue;
+			}
+
+			++it;
+		}
+	}
+
+	const std::vector<IceCube*>& Game::GetActiveGameObjects() const
+	{
+		return mActiveGameObjects;
+	}
+
+	ObjectPool<IceCube>& Game::GetObjectPool()
+	{
+		return mIceCubePool;
+	}
+
+} // namespace
---
+++ Lab9/ObjectPool.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <list>
+#include <cassert>
+
+namespace lab9
+{
+	template <class T>
+	class ObjectPool final
+	{
+	public:
+		ObjectPool(size_t maxPoolSize);
+		ObjectPool(const ObjectPool& other) = delete;
+		~ObjectPool();
+
+		ObjectPool& operator=(const ObjectPool& rhs) = delete;
+		T* Get();
+		void Return(T* instance);
+		inline size_t GetFreeObjectCount() const;
+		inline size_t GetMaxFreeObjectCount() const;
+
+
+	private:
+		using objPtrPool = std::list<T*>;
+
+		size_t mMaxPoolSize;
+		objPtrPool mPool;
+	};
+
+
+	// public method
+
+	template <class T>
+	ObjectPool<T>::ObjectPool(size_t maxPoolSize)
+		: mMaxPoolSize(maxPoolSize)
+	{
+	}
+
+	template <class T>
+	ObjectPool<T>::~ObjectPool()
+	{
+		// pool에 저장된 개체를 모두 지웁니다.
+		for (auto& instance : mPool)
+		{
+			delete instance;
+		}
+
+		mPool.clear();
+	}
+
+	template <class T>
+	T* ObjectPool<T>::Get()
+	{
+		// Pool에서 가장 오래 저장된 T* 을 반환합니다.
+		// Pool이 비어있으면 새로운 T*을 생성하여 반환합니다.
+
+		if (!mPool.empty())
+		{
+			T* oldObj = mPool.front();
+			mPool.pop_front();
+
+			return oldObj;
+		}
+
+		return new T();
+	}
+
+	template <class T>
+	void ObjectPool<T>::Return(T* instance)
+	{
+		// instance를 pool에 저장합니다.
+		// pool이 가득 찼다면 instance를 지웁니다.
+
+		if (mPool.size() == mMaxPoolSize)
+		{
+			delete instance;
+			return;
+		}
+
+		assert(mPool.size() < mMaxPoolSize);
+		mPool.push_back(instance);
+	}
+
+	template <class T>
+	inline size_t ObjectPool<T>::GetFreeObjectCount() const
+	{
+		return mPool.size();
+	}
+
+	template <class T>
+	inline size_t ObjectPool<T>::GetMaxFreeObjectCount() const
+	{
+		return mMaxPoolSize;
+	}
+
+} // namespace 
